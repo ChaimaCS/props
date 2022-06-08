1f650f6cdbfb580fab526601bb1dc6e02d4ec680
ChaimaCS <chaimasouilah11@gmail.com>
2022-06-08 12:12:58 +0100

    props

--- props/src/App.js
+++ props/src/App.js
@@ -4,13 +4,16 @@ import React from 'react';
 import Profile from './profile/ProfileComponent';
 import images from './profile/image.jpg'
 
-
+const handleName = (event) => {
+  event.preventDefault();
+  alert("Chaima SOUILAH");
+};  
 
 function App() {
   
   return (
     <div className="App">
-    <Profile  fullname = "chaima Souilah" profession="Georesources and Environment Engineer" bio="Motivated, hardworking person with a geological and environmental diploma who is currently learning how to code "  >
+    <Profile  fullname = "chaima Souilah" profession="Georesources and Environment Engineer" bio="Motivated, hardworking person with a geological and environmental diploma who is currently learning how to code " alerts={handleName} >
     <img src={images} className="avatar" alt="images"/>
     </Profile>
       
--- props/src/profile/ProfileComponent.jsx
+++ props/src/profile/ProfileComponent.jsx
@@ -1,9 +1,4 @@
-// const Profile = ({fullname, bio, profession} )=>{
-    const handleClick = (event) => {
-        event.preventDefault();
-        alert("Chaima SOUILAH");
-      };    
-const Profile = (props )=>{
+    const Profile = (props )=>{
     console.log(props);
     
     return(
@@ -15,7 +10,7 @@ const Profile = (props )=>{
     <h3>{props.profession}</h3>
 
     <p className="card-text">{props.bio}</p>
-    <a href="/" className="btn btn-primary" onClick={handleClick}>User Name</a>
+    <a href="/" className="btn btn-primary" onClick={props.alerts}>User Name</a>
   </div>
 </div>
    </div>
