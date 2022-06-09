8a7052fcd8bc5bfbefaacb3260484cd04db3328c
ChaimaCS <chaimasouilah11@gmail.com>
2022-06-09 15:22:01 +0100

    props

--- props/src/profile/ProfileComponent.jsx
+++ props/src/profile/ProfileComponent.jsx
@@ -1,21 +1,36 @@
-    const Profile = (props )=>{
-    console.log(props);
+  import PropTypes from 'prop-types';
+  
+const Profile = ({children,fullname,bio,profession,alerts})=>{
     
     return(
         <div>
 <div className="card" style={{width: '18rem'}}>
-{props.children}
+{children}
   <div className="card-body">
-    <h5 className="card-title">{props.fullname}</h5>
-    <h3>{props.profession}</h3>
+    <h5 className="card-title">{fullname}</h5>
+    <h3>{profession}</h3>
 
-    <p className="card-text">{props.bio}</p>
-    <a href="/" className="btn btn-primary" onClick={props.alerts}>User Name</a>
+    <p className="card-text">{bio}</p>
+    <a href="/" className="btn btn-primary" onClick={alerts}>User Name</a>
   </div>
 </div>
    </div>
     )
 }
+
+Profile.propTypes={
+  fullname: PropTypes.string.isRequired,
+  bio: PropTypes.string.isRequired,
+  profession: PropTypes.string.isRequired,
+}
+
+
+Profile.defaultProps = {
+  fullname: "Chaima SOUILAH",
+  bio: "Motivated, hardworking person with a geological and environmental diploma who is currently learning how to code ",
+  profession:"Georesources and Environment Engineer"
+};
+
 
 export default Profile
 
